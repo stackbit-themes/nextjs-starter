5755b2176c8f644feaacae0b5db12981a4c80c5d
Sean C Davis <scdavis41@gmail.com>
2022-03-01 09:24:07 -0500

    Get multiple models working in minimal example

--- minimal/content/pages/index.js
+++ minimal/content/pages/index.js
@@ -4,24 +4,4 @@ export const index = {
   body: "Velit sunt ea ipsum nostrud cupidatat ipsum ex Lorem.",
   __metadata: { id: "content/pages/index.json", urlPath: "/", name: "index" },
 }
-export const new_page = {
-  layout: "Page",
-  title: "This is a new page",
-  body: "Amet exercitation nulla nostrud et ipsum voluptate.",
-  __metadata: {
-    id: "content/pages/new-page.json",
-    urlPath: "/new-page",
-    name: "new_page",
-  },
-}
-export const page_02 = {
-  layout: "Page",
-  title: "Another Page",
-  body: "Ullamco laboris duis incididunt mollit proident est eiusmod.",
-  __metadata: {
-    id: "content/pages/page-02.json",
-    urlPath: "/page-02",
-    name: "page_02",
-  },
-}
-export const allPages = [index, new_page, page_02]
+export const allPages = [index]

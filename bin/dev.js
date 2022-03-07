02f22563d082331540c72e6d88ba243ff68e89bf
Sean C Davis <scdavis41@gmail.com>
2022-03-07 12:32:54 -0500

    Add script that opens the browser when running dev

---
+++ bin/dev.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const open = require("open");
+const { exec } = require("child_process");
+
+const command =
+  "./node_modules/.bin/npm-run-all --parallel next:dev stackbit:dev";
+
+const devProcess = exec(command, (err) => {
+  console.error(`Failed to execute ${command}`, err);
+});
+
+devProcess.stdout.on("data", (data) => {
+  const urlMatching = data.match(
+    /(https:\/\/app\.stackbit\.com\/local\/.*?)\s/
+  );
+
+  if (urlMatching) {
+    console.log(`Opening local editor in browser (${urlMatching[1]})`);
+    open(urlMatching[1]);
+  } else {
+    console.log(data.trim());
+  }
+});

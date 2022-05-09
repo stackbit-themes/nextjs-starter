86c9f205f337df8b130e485621993022eac5b991
tomasbankauskas <tbdizainas@gmail.com>
2022-05-09 15:53:14 +0300

    updated styles, added bg.svg

--- pages/[[...slug]].js
+++ pages/[[...slug]].js
@@ -14,16 +14,6 @@ const FlexiblePage = ({ page, footer }) => {
   return (
     <div className="page-container">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500&display=swap"
-          rel="stylesheet"
-        />
         <title>{page.frontmatter.title}</title>
       </Head>
 
